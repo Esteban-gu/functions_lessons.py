074103aaa9d8a6deee960a56c51060b72149f902
Esteban-gu <esteban2007alvarez@gmail.com>
2025-01-06 12:50:03 -0600

    Add files via upload

--- 3_return.py
+++ 3_return.py
@@ -1,11 +1,11 @@
 # Return Practice #1
 # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, using the first number as the base, and the second as the exponent
 
+def power(base, exponent):
+    power = base ** exponent
+    return power
 
-
-
-
-
+print(power(4, 5))
 
 # Return Practice #2
 # Create a function called usd_to_eur that takes a numeric value (an amount in US dollars) as its only parameter, and returns the equivalent amount in euros as a result. For the purposes of this example, we will take the conversion 1 USD = 0.90 EUR.
@@ -14,8 +14,11 @@
 
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
 
+def multiply(usd, eur):
+    euros = usd * eur
+    return euros
 
-
+print(multiply(5, 0.90))
 
 # Return Practice #3
 # Create a function called reverse_word that takes the characters of a given word as an argument, reverses the order of their characters, and returns them that way and in uppercase.
@@ -25,3 +28,11 @@
 # Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
 
 # Hint: inside the created function, you should use string methods already seen.
+
+def reverse_slicing(word):
+    return word[::-1]
+
+def reverse_word(word):
+    drow = reverse_slicing(word)
+    return drow
+print(reverse_word("school"))
