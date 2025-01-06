69e5e31aef4312e7bcef2c4acdf5a025b31766f0
Esteban-gu <esteban2007alvarez@gmail.com>
2025-01-06 12:28:19 -0600

    Add files via upload

--- 1_.py
+++ 1_.py
@@ -1,34 +1,59 @@
-# Methods, Help & Documentation Practice #1
-# Remove the characters to the left of our main text:
 
-# ,
+# functions are ways to wrap your code
+# into reusable units
 
-# :
+# function = A block of reusable code
+#            place () after the function name to invoke it
 
-# %
+#   define a function
+#   parameter is a placeholder while you call the function
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy birthday to you!")
+    print()
 
-# _
+#   call a function
+#   an argument is where it keeps all the information to display in the parameter
+happy_birthday("Bro", 20)
+happy_birthday("ChoppedChin", 157)
+happy_birthday("Vexbolts", 40)
 
-# #
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}")
 
-# Use the lstrip() method. Print the result to the screen:
+display_invoice("JoeSchmo", 100.01, "01/02")
 
-# ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
+#   return = statement used to end a function
+#           and send a result back to the caller
 
-# Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+def add(x, y):
+    z = x + y
+    return z
 
+def subtract(x, y):
+    z = x - y
+    return z
 
-# Methods, Help & Documentation Practice #2
-# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+def multiply(x, y):
+    z = x * y
+    return z
 
-# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+def divide(x, y):
+    z = x / y
+    return z
 
-# Search the documentation for the requested method to know how it works.
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(1, 2))
+print(divide(1, 2))
 
-# Methods, Help & Documentation Practice #3
-# Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
 
-# phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-# tv_brands = {"Sony", "Philips", "Samsung", "LG"}
-# Search the documentation for the requested method to know how it works.
+full_name = create_name("spongebob", "squarepants")
 
+print(full_name)
