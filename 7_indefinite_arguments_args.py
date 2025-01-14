6094e45140b12940e2fde7bbd48b071bf073ceaf
Esteban-gu <esteban2007alvarez@gmail.com>
2025-01-14 12:13:00 -0600

    Add files via upload

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,73 @@
+# *args = allows you to pass multiple non-key arguments
+# **kwargs = allows you to pass multiple keyword-arguments
+# * unpacking operator
+# 1. positional 2. default 3. keyword 4. ARBITRARY
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ")
+    
+# display_name("Dr.", "Spongebob", "Harold", "Squarepants", "III")
+
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
+
+# print_address(street="123 Evins St.", city="Chicago", state="Illinois", zip="60632")
+
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    for value in kwargs.values():
+        print(value, end=" ")
+
+shipping_label("Dr. ", "Spongebob", "Squarepants", "III",
+               street="123 Evins St.", 
+               apt="470",
+               city="Chicago",
+               state="IL",
+               zip="54321")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
